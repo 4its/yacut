79cb4fd482876b704163300a8f300b6c56256b0d
Goerge Egiazaryan <egiazaryan@gmail.com>
2024-07-26 21:50:37 +0300

    fix get_unique_short_id;

--- yacut/utils.py
+++ yacut/utils.py
@@ -2,8 +2,9 @@ from random import choices
 from string import ascii_letters, digits
 
 
-AVAILIBLE_CHARS = set(ascii_letters + digits)
+AVAILIBLE_CHARS = ascii_letters + digits
 DEFAULT_LENGTH = 6
+
 
 def get_unique_short_id():
     return ''.join(choices(AVAILIBLE_CHARS, k=DEFAULT_LENGTH))
