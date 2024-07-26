46e50428110882df00c0399cddf4f156b8b7b213
Goerge Egiazaryan <egiazaryan@gmail.com>
2024-07-26 21:51:06 +0300

    done with forms;

--- yacut/forms.py
+++ yacut/forms.py
@@ -1,21 +1,31 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, TextAreaField, URLField
-from wtforms.validators import DataRequired, Length, Optional
+from wtforms import StringField, SubmitField, URLField
+from wtforms.validators import DataRequired, Length, Optional, Regexp, URL
+
+REQUIRED_FIELD = 'Обязательное поле'
+WRONG_URL = 'Неправильный URL'
+LINK_LABEL = 'Длинная ссылка'
+ID_LABEL = 'Ваш вариант короткой ссылки'
+WRONG_CHARS = 'Недопустимые символы в короткой ссылке'
+SUBMIT_LABEL = 'Создать'
+CUSTOM_ID_PATTERN = r'^[A-Za-z0-9]+$'
 
 
 class URLMapForm(FlaskForm):
-    original_link = StringField(
-        'Оригинальная ссылка',
+    original_link = URLField(
+        LINK_LABEL,
         validators=(
-            DataRequired(message='Обязательное поле'),
-            Length(1, 256)
+            DataRequired(message=REQUIRED_FIELD),
+            Length(1, 256),
+            URL(require_tld=True, message=WRONG_URL),
         )
     )
     custom_id = StringField(
-        'Пользовательские идентификатор',
+        ID_LABEL,
         validators=(
-            Optional,
-            Length(1, 16)
+            Length(1, 16),
+            Optional(),
+            Regexp(CUSTOM_ID_PATTERN, message=WRONG_CHARS),
         )
     )
-    submit = SubmitField('Создать')
+    submit = SubmitField(SUBMIT_LABEL)
