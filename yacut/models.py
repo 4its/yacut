eb5cb2e7e623a14efe1623a0aa18d75ef50c0daf
Goerge Egiazaryan <egiazaryan@gmail.com>
2024-07-27 20:29:55 +0300

    fix for older versions;

--- yacut/models.py
+++ yacut/models.py
@@ -1,4 +1,4 @@
-from datetime import datetime, UTC
+from datetime import datetime
 
 from flask import url_for
 
@@ -9,7 +9,7 @@ class URLMap(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     original = db.Column(db.String(256), nullable=False)
     short = db.Column(db.String(16), unique=True)
-    timestamp = db.Column(db.DateTime, index=True, default=datetime.now(UTC))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
 
     def to_dict(self):
         return dict(
