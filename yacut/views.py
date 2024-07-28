9156076f0e2a0191815efc836ecd1314a1474acb
Goerge Egiazaryan <egiazaryan@gmail.com>
2024-07-28 14:08:42 +0300

    del flash message;  - cleanup some mistakes from review;

--- yacut/views.py
+++ yacut/views.py
@@ -10,23 +10,26 @@ from settings import TextErrors
 @app.route('/', methods=['GET', 'POST'])
 def index_view():
     form = URLMapForm()
-    if form.validate_on_submit():
-        custom_id = form.custom_id.data
-        if not custom_id:
-            custom_id = get_unique_short_id()
-        elif URLMap.query.filter_by(short=custom_id).first():
-            form.custom_id.errors = (TextErrors.LABEL_EXIST,)
-            return render_template('cut_form.html', form=form)
-        db.session.add(
-            URLMap(
-                original=form.original_link.data,
-                short=custom_id,
-            )
+    link = None
+    if not form.validate_on_submit():
+        return render_template('index.html', form=form, )
+    short = form.custom_id.data
+    if not short:
+        short = get_unique_short_id()
+    elif URLMap.query.filter_by(short=short).first():
+        form.custom_id.errors = (TextErrors.LABEL_EXIST,)
+        return render_template('index.html', form=form)
+    db.session.add(
+        URLMap(
+            original=form.original_link.data,
+            short=short,
         )
-        db.session.commit()
-        link = url_for('redirect_to', short=custom_id, _external=True)
-        flash(f'<a href="{link}" class="text-center">{link}</a>')
-    return render_template('cut_form.html', form=form)
+    )
+    db.session.commit()
+    return render_template(
+        'index.html', form=form,
+        link=url_for('redirect_to', short=short, _external=True)
+    )
 
 
 @app.route('/<short>', methods=['GET'])
