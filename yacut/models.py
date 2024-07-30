e810b7a5be871ef7b56268d6c6830c5a1df01a66
Goerge Egiazaryan <egiazaryan@gmail.com>
2024-07-30 10:17:43 +0300

    fix after second review;  - changed some names;  - changed logic;

--- yacut/models.py
+++ yacut/models.py
@@ -5,11 +5,10 @@ from re import match
 from flask import url_for
 
 from settings import (
-    AVAILIBLE_CHARS, DEFAULT_LENGTH, ORIGINAL_LENGTH,
+    AVAILIBLE_CHARS, DEFAULT_SHORT_LENGTH, MAX_ATTEMPTS, ORIGINAL_LENGTH,
     SHORT_LENGTH, SHORT_PATTERN, REDIRECT_FUNCTION, TextErrors
 )
 from yacut import db
-from .error_handlers import InvalidAPIUsage
 
 
 class URLMap(db.Model):
@@ -21,32 +20,36 @@ class URLMap(db.Model):
     def short_link(self):
         return url_for(REDIRECT_FUNCTION, short=self.short, _external=True)
 
-    def to_dict(self, short=False):
-        if short:
+    def to_dict(self, original_only=False):
+        if original_only:
             return dict(url=self.original)
         return dict(url=self.original, short_link=self.short_link())
 
     @staticmethod
-    def check_short(short, or_404=False):
-        if not or_404:
-            return URLMap.query.filter_by(short=short).first()
-        return URLMap.query.filter_by(short=short).first_or_404()
+    def get_object(short, or_404=False):
+        if or_404:
+            return URLMap.query.filter_by(short=short).first_or_404()
+        return URLMap.query.filter_by(short=short).first()
 
     @staticmethod
     def generate_short():
-        while True:
-            short = ''.join(choices(AVAILIBLE_CHARS, k=DEFAULT_LENGTH))
-            if not URLMap.check_short(short):
+        for _ in range(MAX_ATTEMPTS):
+            short = ''.join(choices(AVAILIBLE_CHARS, k=DEFAULT_SHORT_LENGTH))
+            if not URLMap.get_object(short):
                 return short
+        raise RuntimeError(TextErrors.GENERATION_ERROR)
 
     @staticmethod
-    def add_short(original, short=None):
-        if short is None:
+    def create_urlmap(original, short=None, from_form=False):
+        if not short:
             short = URLMap.generate_short()
-        elif URLMap.check_short(short):
-            raise InvalidAPIUsage(TextErrors.LABEL_EXIST)
-        elif not match(SHORT_PATTERN, short) or len(short) > SHORT_LENGTH:
-            raise InvalidAPIUsage(TextErrors.BAD_CUSTOM_ID)
+        if not from_form:
+            if len(short) > SHORT_LENGTH or not match(SHORT_PATTERN, short):
+                raise ValueError(TextErrors.BAD_CUSTOM_ID)
+            if URLMap.get_object(short):
+                raise ValueError(TextErrors.ID_ALREADY_EXIST)
+            if len(original) > ORIGINAL_LENGTH:
+                raise ValueError(TextErrors.BAD_ORIGINAL_LENGTH)
         url_map = URLMap(original=original, short=short)
         db.session.add(url_map)
         db.session.commit()
